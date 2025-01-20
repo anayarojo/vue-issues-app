6b2b2ad8dcfc29f0150778d2fb9a2f4fae6fe6ff
anayarojo <raulmartin_04@hotmail.com>
2025-01-19 21:25:01 -0700

    feat: add FilterSelector, LabelSelector, and StateSelector components for issue filtering

---
+++ src/issues/components/filter-selector/LabelSelector.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import useLabels from 'src/issues/composables/useLabels'
+import IssueLabel from 'src/issues/components/IssueLabel.vue'
+import LoaderSpinner from 'src/shared/components/LoaderSpinner.vue'
+
+const { isLoading, labels, toggleLabel, isSelectedLabel } = useLabels()
+</script>
+
+<template>
+  <div class="q-mt-md">
+    <LoaderSpinner v-if="isLoading" size="50px" :thickness="3" :show-text="false" />
+    <IssueLabel
+      v-for="label in labels"
+      :key="label.id"
+      :label="label"
+      :dark-mode="true"
+      :is-selected="isSelectedLabel(label.name)"
+      @toggle-label="(labelName: string) => toggleLabel(labelName)"
+    ></IssueLabel>
+  </div>
+</template>
---
+++ src/issues/components/filter-selector/StateSelector.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { State } from 'src/issues/interfaces/issue.interface'
+import { useIssuesStore } from 'src/stores/issues.store'
+
+const issuesStore = useIssuesStore()
+
+const states = [
+  { label: 'Open', value: State.Open },
+  { label: 'Closed', value: State.Closed },
+  { label: 'All', value: State.All },
+]
+</script>
+
+<template>
+  <q-btn-toggle v-model="issuesStore.state" toggle-color="primary" :options="states" spread />
+</template>
