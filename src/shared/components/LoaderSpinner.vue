55609664919ae6a50bcd739a3ea5c45920046c6d
anayarojo <raulmartin_04@hotmail.com>
2025-01-19 21:24:50 -0700

    feat: add LoaderSpinner and SidebarLink components for improved UI elements

---
+++ src/shared/components/LoaderSpinner.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+defineProps({
+  color: {
+    type: String,
+    default: 'primary',
+  },
+  size: {
+    type: String,
+    default: '3rem',
+  },
+  thickness: {
+    type: Number,
+    default: 5,
+  },
+  text: {
+    type: String,
+    default: 'Loading...',
+  },
+  showText: {
+    type: Boolean,
+    default: true,
+  },
+})
+</script>
+
+<template>
+  <div class="column items-center">
+    <q-spinner :color="color" :size="size" :thickness="thickness" />
+    <span v-if="showText">{{ text }}</span>
+  </div>
+</template>
---
+++ src/shared/components/SidebarLink.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+export interface SidebarLinkProps {
+  active?: boolean
+  title: string
+  link?: string
+  icon?: string
+}
+
+withDefaults(defineProps<SidebarLinkProps>(), {
+  active: false,
+  link: '',
+  icon: '',
+})
+</script>
+
+<template>
+  <router-link :to="link || '/'">
+    <q-item clickable :active="active" v-ripple>
+      <q-item-section v-if="icon" avatar>
+        <q-icon :name="icon" />
+      </q-item-section>
+      <q-item-section>
+        {{ title }}
+      </q-item-section>
+    </q-item>
+  </router-link>
+</template>
+
+<style>
+a {
+  text-decoration: none;
+}
+</style>
